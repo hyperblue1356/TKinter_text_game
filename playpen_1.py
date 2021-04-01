38705ee04483bc14af92748b4fb47d6b73d256c5
hyperblue1356 <69151435+hyperblue1356@users.noreply.github.com>
2021-04-01 19:39:14 +0400

    Updates to text_game_tkinter

---
+++ playpen_1.py
@@ -0,0 +1,58 @@
+# Make questions and input box to type answer, answer saved to player info
+# Create a function to retrieve data from result box and save to player data.
+
+try:
+    import tkinter
+except ImportError:  # python 2
+    import Tkinter as tkinter
+
+import time
+import os
+
+mainWindow = tkinter.Tk()
+mainWindow.title = "Radiants"
+mainWindow.geometry = "640x480"
+
+question_list = ["I am the remnants of Honour, What is your name?", "What is your age?",
+                 "Choose a race you would like to know more about",
+                 "Would you like to know about another race?: ",
+                 "What race are you?: ",
+                 "Choose an order you would like to know more about: ",
+                 "Would you like to know about another order?: ",
+                 "What order are you?: ", ]
+
+player_info = []
+
+
+def counter():
+    global question_list
+
+    my_label.config(text=question_list.pop(0))
+
+def create_my_button():
+    start.destroy()
+    my_button = tkinter.Button(frame1, text="Yes", command=counter)
+    my_button.grid(row=1, column=0)
+
+
+
+canvas1 = tkinter.Canvas(mainWindow, width=640, height=480)
+canvas1.grid(row=0, column=0, rowspan=2, sticky="nsew")
+
+frame1 = tkinter.Frame(canvas1, relief="sunken", borderwidth=1, )
+frame1.grid(row=0, column=0, sticky='EW')
+
+my_label = tkinter.Label(frame1, background="White")
+my_label.grid(row=0, column=0)
+
+start = tkinter.Button(frame1, text="Start", command=create_my_button)
+start.grid(row=1, column=0)
+
+
+resultLabel = tkinter.Label(mainWindow, text="Result")
+resultLabel.grid(row=1, column=2, sticky='nw')
+result = tkinter.Entry(mainWindow)
+result.grid(row=2, column=2, sticky='sw')
+
+# Start the GUI
+mainWindow.mainloop()
